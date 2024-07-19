f17f91374d819086c0626136260e6c88705a17de
VerneriLahteenoja <verneri.lahteenoja@gmail.com>
2024-07-19 21:06:17 +0300

    create Part types for different course structures

--- vite_ts/src/types/types.ts
+++ vite_ts/src/types/types.ts
@@ -3,16 +3,47 @@ export interface HeaderProps {
 }
 
 // Individual course object
-export type Course = {
+interface CoursePartBase {
   name: string;
   exerciseCount: number;
-};
+}
+
+interface CoursePartDescription extends CoursePartBase {
+  description: string;
+}
+
+// Description, Name and Exercise Count
+interface CoursePartBasic extends CoursePartDescription {
+  kind: "basic"
+}
+
+interface CoursePartBackground extends CoursePartDescription {
+  backgroundMaterial: string;
+  kind: "background"
+}
+
+interface CoursePartRequirement extends CoursePartDescription {
+  requirements: string[],
+  kind: "special"
+}
+
+// No Description
+interface CoursePartGroup extends CoursePartBase {
+  groupProjectCount: number;
+  kind: "group"
+}
+
+export type CoursePart = CoursePartBasic | CoursePartGroup | CoursePartBackground | CoursePartRequirement;
 
 // An Array of Course objects
 export interface ContentProps {
-  courseParts: Course [];
+  courseParts: CoursePart [];
 }
 
 export interface TotalProps {
   totalExercises: number;
+}
+
+export interface PartProps {
+  coursePart: CoursePart
 }
